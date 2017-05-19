ae3a5282055f049779b7dc82122148e1599af2f9
Manuel Koch <mko0815@gmail.com>
2017-05-19 12:51:17 +0200

    Add license and readme files to bundle

--- bundle.sh
+++ bundle.sh
@@ -9,15 +9,24 @@ export PATH=${QT5DIR}:${PYQT5_DIR}:${PATH}
 echo PYQT5_DIR=${PYQT5_DIR}
 echo QT5DIR=${QT5DIR}
 
+THIS_DIR=$(dirname $0)
+BUNDLE_CONTENTS_DIR=${THIS_DIR}/dist/GitOver.app/Contents
+BUNDLE_MACOS_DIR=${BUNDLE_CONTENTS_DIR}/MacOS
+
 # When using pyenv virtualenv the Python interpreter must be build with shared option enabled.
 # $ env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.5.3
 # See https://github.com/pyinstaller/pyinstaller/wiki/Development
 #
 # Extended debug while building/running application:
 # --log-level=DEBUG --debug
-pyinstaller --icon $(dirname $0)/res/icon.icns --onefile --windowed --noconfirm --clean -n GitOver --paths $(dirname $0) $(dirname $0)/gitover/main.py
+pyinstaller --icon ${THIS_DIR}/res/icon.icns --onefile --windowed --noconfirm --clean -n GitOver --paths ${THIS_DIR} ${THIS_DIR}/gitover/main.py
 
 # Add support for high DPI aka retina displays
-INFO_PLIST=$(dirname $0)/dist/GitOVer.app/Contents/info.plist
+INFO_PLIST=${BUNDLE_CONTENTS_DIR}/Info.plist
 plutil -insert NSPrincipalClass -string NSApplication ${INFO_PLIST}
 plutil -insert NSHighResolutionCapable -string True ${INFO_PLIST}
+
+# Add various info texts too
+cp ${THIS_DIR}/COPYING ${BUNDLE_MACOS_DIR}
+cp ${THIS_DIR}/LICENSE ${BUNDLE_MACOS_DIR}
+cp ${THIS_DIR}/README.md ${BUNDLE_MACOS_DIR}
