527909282aab50cfb39f24d1207da3d3b35d3123
Manuel Koch <mko0815@gmail.com>
2017-07-18 08:26:28 +0200

    Simplify diff highlighter

--- gitover/formatter.py
+++ gitover/formatter.py
@@ -24,23 +24,8 @@ class GitDiffHightlighter(QSyntaxHighlighter):
     def __init__(self, doc=None):
         super().__init__(doc)
 
-    def _getColoredFmt(self, color):
-        """Returns QTextCharFormat to represent named color"""
-        fmt = {"bold": True}
-        if not QColor.isValidColor(color):
-            color = color.replace("#", "")
-        if QColor.isValidColor(color):
-            fmt["bg"] = color
-            bgcolor = QColor(color)
-            bgv = sum([bgcolor.redF(), bgcolor.greenF(), bgcolor.blueF()]) / 3
-            if bgv > 0.55:
-                fmt["fg"] = "black"
-            else:
-                fmt["fg"] = "white"
-        return fmt
-
     def highlightBlock(self, text):
-        "Apply highlighting format by analyzing given text"
+        """Apply highlighting format by analyzing given text"""
         self._applyFormat(text, self.HEADER_DIFF_RE, {"fg": "blue"})
         self._applyFormat(text, self.HEADER_INDEX_RE, {"fg": "blue"})
         self._applyFormat(text, self.HEADER_NAME_A_RE, {"fg": "red"})
@@ -50,7 +35,6 @@ class GitDiffHightlighter(QSyntaxHighlighter):
         self._applyFormat(text, self.HEADER_CHANGE_B_RE, {"fg": "green"})
 
     def _applyFormat(self, text, regexp, fmt):
-        fontPt = self.document().defaultFont().pointSize()
         for m in regexp.finditer(text):
             fmt_ = fmt(m.group(0)) if callable(fmt) else fmt
             if not fmt_:
@@ -58,31 +42,12 @@ class GitDiffHightlighter(QSyntaxHighlighter):
             grp = "subject" if "subject" in m.groupdict() else 0
             s = m.start(grp)
             e = m.end(grp)
-            for i in range(s, e):
-                charfmt = QTextCharFormat()
-                if fmt_.get("italic", False):
-                    charfmt.setFontItalic(True)
-                if fmt_.get("bold", False):
-                    charfmt.setFontWeight(QFont.Bold)
-                if fmt_.get("underline", False):
-                    charfmt.setFontUnderline(True)
-                if fmt_.get("wave", False):
-                    charfmt.setFontUnderline(True)
-                if fmt_.get("stroke", False):
-                    charfmt.setFontStrikeOut(True)
-                if fmt_.get("monospace", False):
-                    charfmt.setFontFamily("monospace")
-                if fmt_.get("size", 0):
-                    charfmt.setFontPointSize(fontPt + fmt_.get("size"))
-                else:
-                    charfmt.setFontPointSize(max(self.format(i).font().pointSize(), fontPt))
-                if fmt_.get("fg", "") and QColor.isValidColor(fmt_.get("fg")):
-                    charfmt.setForeground(QBrush(QColor(fmt_.get("fg"))))
-                else:
-                    charfmt.setForeground(self.format(i).foreground())
-                if fmt_.get("bg", "") and QColor.isValidColor(fmt_.get("bg")):
-                    charfmt.setBackground(QBrush(QColor(fmt_.get("bg"))))
-                self.setFormat(i, 1, charfmt)
+            charfmt = QTextCharFormat()
+            if fmt_.get("fg", "") and QColor.isValidColor(fmt_.get("fg")):
+                charfmt.setForeground(QBrush(QColor(fmt_.get("fg"))))
+            if fmt_.get("bg", "") and QColor.isValidColor(fmt_.get("bg")):
+                charfmt.setBackground(QBrush(QColor(fmt_.get("bg"))))
+            self.setFormat(s, e-s, charfmt)
 
 
 class GitDiffFormatter(QObject, QmlTypeMixin):
